4ae07717392a87e15672a573398328618575dd71
PavanPenumajji <pavan.penumajji@techolution.com>
2025-05-28 13:38:07 +0530

    Delete AppConfig.java

--- AppConfig.java
+++
@@ -1,8 +0,0 @@
-public class AppConfig {
-    public static final String APP_NAME = "MyJavaApp";
-    public static final int VERSION = 1;
-
-    public static void printConfig() {
-        System.out.println("App: " + APP_NAME + ", Version: " + VERSION);
-    }
-}
